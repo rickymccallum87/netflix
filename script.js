4af62c8ff5e54deebe61ec2a976f66f1f84455af
Ricky McCallum <contact@rickymccallum.com>
2020-11-08 11:43:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var acc = document.getElementsByClassName("faq-question");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var answer = this.nextElementSibling;
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+}
